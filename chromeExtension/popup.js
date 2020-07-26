7bb77807a8e64c9e00934788e1b5952dad94de9d
Karen Gu <karengu@Karens-MBP-4.home>
2020-07-25 22:43:17 -0400

    Add ability to add new task with NOT STARTED status

--- chromeExtension/popup.js
+++ chromeExtension/popup.js
@@ -20,48 +20,20 @@ document.addEventListener('DOMContentLoaded', function() {
     }, false);
 
 
-    chrome.identity.getProfileUserInfo(function(userinfo){
-
-	var  uniqueId=userinfo.email.split("@")[0];
-	var  uid = -1;
-	var total = 0;
+    requestWithUser(function(uid) {
+	// update server with info on current tab
+	var xhr1 = new XMLHttpRequest();
+	xhr1.open("PUT", 'http://localhost:8080/api/users/'.concat(uid).concat('/'), true);
+	xhr1.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	
-	// code to GET users
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "http://localhost:8080/api/users", true);
-	xhr.onreadystatechange = function () {
-	    if (xhr.readyState !== 4) {
-		return
-	    }
-            var data = JSON.parse(xhr.responseText);
-            for (object in data) {
-		if (data[object]["username"] === uniqueId) {
-                    uid = data[object]["id"];
-		}
-		total = data[object]["id"];
-            }
-
-	    var xhr1 = new XMLHttpRequest();
-	    if (uid != -1) {
-
-		// update server with info on current tab
-		var xhr1 = new XMLHttpRequest();
-		xhr1.open("PUT", 'http://localhost:8080/api/users/'.concat(uid).concat('/'), true);
-		xhr1.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		
-		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-		    var url = tabs[0].url;
-		    var d = new Date();
-		    var a = "id=".concat(uid);
-		    var b = "&lastVisitedTime=".concat(d.toLocaleDateString());
-		    var c = "&lastVisitedUrl=".concat(url);
-		    xhr1.send(a.concat(b).concat(c));
-		});
-	    }
-
-	    
-	}
-	xhr.send();
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+	    var url = tabs[0].url;
+	    var d = new Date();
+	    var a = "id=".concat(uid);
+	    var b = "&lastVisitedTime=".concat(d.toLocaleDateString());
+	    var c = "&lastVisitedUrl=".concat(url);
+	    xhr1.send(a.concat(b).concat(c));
+	});
     })
     
 }, false);
--- chromeExtension/tasks.js
+++ chromeExtension/tasks.js
@@ -1,30 +1,55 @@
+var insertRow = function(task, table) {
+    var row = table.insertRow(-1);
+
+    var nameCell = row.insertCell(0);
+    nameCell.innerHTML = task.userId;
+
+    var urlCell = row.insertCell(1);
+    urlCell.innerHTML = task.description
+
+    var timeCell = row.insertCell(2);
+    timeCell.innerHTML = task.status
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  // code to GET users
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://localhost:8080/api/tasks", true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4) {
-        var taskPages = JSON.parse(xhr.responseText);
-	var i;
-	var table = document.getElementById("friendBrowsers");
+    // code to GET users
+    var table = document.getElementById("friendBrowsers");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080/api/tasks", true);
+    xhr.onreadystatechange = function () {
+	if (xhr.readyState == 4) {
+            var taskPages = JSON.parse(xhr.responseText);
+	    var i;
 
-	for (i = 0; i < taskPages.length; i++) {
-  	    var row = table.insertRow(-1);
-
-            var url ="http://localhost:8080/api/tasks".concat(taskPages[0].userId);
-            fetch(url, {"mode": "no-cors",  "method":"GET"}).then(response => { alert(response); });
-
-	    var nameCell = row.insertCell(0);
-	    nameCell.innerHTML = taskPages[i].userId;
-
-	    var urlCell = row.insertCell(1);
-	    urlCell.innerHTML = taskPages[i].description
-
-	    var timeCell = row.insertCell(2);
-	    timeCell.innerHTML = taskPages[i].status
+	    for (i = 0; i < taskPages.length; i++) {
+		insertRow(taskPages[i], table);		
+	    }
 	}
     }
-  }
-  xhr.send();
+    xhr.send();
 
+    document.getElementById('submitNewTask').addEventListener('click', function() {
+	requestWithUser(function(uid) {
+	    var taskXHR = new XMLHttpRequest();
+	    taskXHR.open("POST", "http://localhost:8080/api/tasks/", true);
+	    taskXHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	    xhr.onreadystatechange = function() {
+		if (xhr.readyState !== 4) {
+		    return
+		}
+		insertRow({
+		    userId: uid,
+		    description: document.getElementById("taskDescription").value,
+		    status: "NOT STARTED"
+		}, table)
+	    }
+	    taskXHR.send("description="
+			 .concat(document.getElementById("taskDescription").value)
+			 .concat("&userId=")
+			 .concat(uid)
+			 .concat("&status=")
+			 .concat("NOT STARTED")
+			)
+	});
+    }, false);
 }, false);
