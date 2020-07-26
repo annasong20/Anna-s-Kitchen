40a4f11846aafee056474ed1bb0e407fde62a5f0
Sarbari Sarkar <sarbari@mit.edu>
2020-07-25 23:53:12 -0400

    added friends

--- chromeExtension/friends.js
+++ chromeExtension/friends.js
@@ -1,65 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-/*
-    chrome.identity.getProfileUserInfo(function(userinfo) {
-        var  uniqueId=userinfo.email.split("@")[0];
-        var  uid = -1;
-        var total = 0;
- 
-        // code to GET users
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8080/api/users", true);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                var data = JSON.parse(xhr.responseText);
-                for (object in data) {
-                    if (data[object]["username"] === uniqueId) {
-                        uid = data[object]["id"];
-                    }
-                    total = data[object]["id"];
-                 }
-
-                var xhr1 = new XMLHttpRequest();
-                if (uid != -1) {
-
-                    var xhr2 = new XMLHttpRequest();
-                    xhr2.open("DELETE", 'http://localhost:8080/api/users/'.concat(uid), true);
-                    xhr2.onreadystatechange = function () {
-                        if (xhr2.readyState == 4) {
-       		            alert("done");
-     		        }
-                    }
-                    xhr2.send("id=".concat(uid));
-                }
-
-                // update server with info on current tab
-                var xhr1 = new XMLHttpRequest();
-                xhr1.open("POST", 'http://localhost:8080/api/users/', true);
-                xhr1.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-     
-                chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-                   var url = tabs[0].url;
-                   var d = new Date();
-                   var uuid = uid;
-                   var a = "username=".concat(uniqueId);
-                   var b = "&lastVisitedTime=".concat(d.toLocaleDateString());
-                   var c = "&lastVisitedUrl=".concat(url);
-                   alert(a.concat(b,c));
-                   xhr1.send(a.concat(b,c));
-                });
-
-            }
-      }
-      xhr.send();
-  });*/
-
 
   chrome.identity.getProfileUserInfo(function(userinfo) {
       var  uniqueId = userinfo.email.split("@")[0];
       
  	
-
-
       // code to GET users
       var xhr1 = new XMLHttpRequest();
       xhr1.open("GET", "http://localhost:8080/api/users", true);
@@ -79,9 +23,32 @@ document.addEventListener('DOMContentLoaded', function() {
 	              nameCell.innerHTML = users[i].username;
 
 	              var addCell = row.insertCell(1);
-	              addCell.innerHTML = "<button id='add'.concat(i)>Add Friend</button>";
+	              addCell.innerHTML = users[i].id;
 		  }
               }
+
+    document.getElementById('submitNewFriend').addEventListener('click', function() {
+        requestWithUser(function(uid) {
+            var taskXHR = new XMLHttpRequest();
+            taskXHR.open("POST", "http://localhost:8080/api/friends/", true);
+            taskXHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState !== 4) {
+                    return
+                }
+                insertRow({
+                    parentId: uId,
+                    siblingId: document.getElementById("username").value,
+                }, table)
+            }
+            taskXHR.send("parentId="
+                         .concat(uId)
+                         .concat("&siblingId=")
+                         .concat(document.getElementById("username").value)
+                        )
+
+        });
+    }, false);
 
 
               if (uId != -1) {
